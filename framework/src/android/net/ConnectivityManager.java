60e077e2857e53b2d7023d660f283317cd2f6d1d
DennySPb <dennyspb@gmail.com>
2022-05-12 13:02:13 +0300

    Revert "gmscompat: Add ConnectivityManager hook for baseline compatibility"
    This reverts commit 36d29f0c2ec92b6d389044cfded441440a508c3f.

--- framework/src/android/net/ConnectivityManager.java
+++ framework/src/android/net/ConnectivityManager.java
@@ -38,7 +38,6 @@ import android.annotation.SystemService;
 import android.annotation.UserIdInt;
 import android.app.PendingIntent;
 import android.app.admin.DevicePolicyManager;
-import android.app.compat.gms.GmsCompat;
 import android.compat.annotation.UnsupportedAppUsage;
 import android.content.ComponentName;
 import android.content.Context;
@@ -2608,10 +2607,6 @@ public class ConnectivityManager {
     @RequiresPermission(anyOf = {android.Manifest.permission.TETHER_PRIVILEGED,
             android.Manifest.permission.WRITE_SETTINGS})
     public boolean isTetheringSupported() {
-        if (GmsCompat.isEnabled()) {
-            return false;
-        }
-
         return getTetheringManager().isTetheringSupported();
     }
 
